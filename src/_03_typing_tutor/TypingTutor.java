36de7ebd6a3c5c0b38d6b35a7a2df00df022492d
league <league@imac-24.attlocal.net>
2023-07-22 16:31:01 -0700

    Start on book of illusions code

--- src/_03_typing_tutor/TypingTutor.java
+++ src/_03_typing_tutor/TypingTutor.java
@@ -1,12 +1,16 @@
 package _03_typing_tutor;
 
+import java.awt.Color;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 import java.util.Random;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 
-public class TypingTutor {
+public class TypingTutor implements KeyListener {
 	JFrame frame = new JFrame();
 	JPanel panel = new JPanel();
 	JLabel label = new JLabel();
@@ -17,10 +21,19 @@ public class TypingTutor {
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		currentLetter = generateRandomLetter();
 		label.setText("" + currentLetter);
+		label.setFont(label.getFont().deriveFont(28.0f));
+		label.setHorizontalAlignment(JLabel.CENTER);
+		frame.addKeyListener(this);
+		panel.add(label);
+		frame.add(panel);
+		
+		
 	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		TypingTutor tutor = new TypingTutor();
+		tutor.setup(); 
 		
 	}
 	char generateRandomLetter() {
@@ -28,5 +41,34 @@ public class TypingTutor {
 	    return (char) (r.nextInt(26) + 'a');
 	}
 
-}
+	@Override
+	public void keyTyped(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
 
+	@Override
+	public void keyPressed(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
+		if (e.getKeyChar() == currentLetter) {
+			System.out.println();
+			panel.setBackground(Color.GREEN);
+			
+		}else {
+			panel.setBackground(Color.RED);
+			
+		}
+		currentLetter = generateRandomLetter();
+		label.setText("" + currentLetter);
+	}
+
+	@Override
+	public void keyReleased(KeyEvent e) {
+		// TODO Auto-generated method stub
+		System.out.println(e.getKeyChar());
+		
+	}
+	
+}
+// Nothing else to work in here
