d992a3b1f5fffc91e19707c81fec348b79b4a6bb
sgupta <suryanigupta10@gmail.com>
2023-09-09 16:24:11 -0700

    finish the slot machine

--- src/_09_whack_a_mole/WhackAMole.java
+++ src/_09_whack_a_mole/WhackAMole.java
@@ -2,6 +2,8 @@ package _09_whack_a_mole;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.Date;
+
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
@@ -14,8 +16,34 @@ public class WhackAMole implements ActionListener {
 		JFrame WAMFrame = new JFrame();
 		JPanel WAMPanel = new JPanel();
 		JLabel WAMLabel = new JLabel();
+		int WAMScore = 0;
+		int WAMMiss = 5;
 		private int buttonCount = 0;
+		Date startDate; 
 		
+	static void speak(String words) {
+	    if( System.getProperty( "os.name" ).contains( "Windows" ) ) {
+	    	String cmd = "PowerShell -Command \"Add-Type -AssemblyName System.Speech; (New-Object System.Speech.Synthesis.SpeechSynthesizer).Speak('"
+	                    + words + "');\"";
+	        try {
+	                Runtime.getRuntime().exec( cmd ).waitFor();
+	        } catch( Exception e ) {
+	                e.printStackTrace();
+	            }
+	    } else {
+	        try {
+	                Runtime.getRuntime().exec( "say " + words ).waitFor();
+	        } catch( Exception e ) {
+	                e.printStackTrace();
+	            }
+	    }
+	}	
+	private void endGame(Date timeAtStart, int molesWhacked) { 
+		Date timeAtEnd = new Date();
+		JOptionPane.showMessageDialog(null, "Your whack rate is " + ((timeAtEnd.getTime() - timeAtStart.getTime()) / 1000.00 / molesWhacked) + " moles per second.");
+	}
+		
+	
 	
 	public static void run(String[] args) {
 		// TODO Auto-generated method stub
@@ -34,7 +62,7 @@ public class WhackAMole implements ActionListener {
 		
 		}
 		public void setting(){
-			
+			startDate = new Date();
 		}
 	public void actionPerformed(ActionEvent e) {
 		
@@ -44,10 +72,29 @@ public class WhackAMole implements ActionListener {
 		JButton buttonPressed = (JButton) e.getSource();
 		// If the buttonPressed was the firstButton
 		if(buttonPressed.getText().equals("mole")) {
-		
+			
+			if (WAMScore == 10) {
+				JOptionPane.showMessageDialog(buttonPressed, "You won!");
+				endGame(startDate, WAMScore);
+				
+			}else { 
+				WAMScore ++;
+				
+			}	
+		}else {
+			if (WAMMiss == 5) {
+				endGame(startDate, WAMScore);
+			}else {
+			WAMMiss --;
+			speak("You Missed Bozo");
+			
+			}
+			}
 			
 		}
 
-	}
+	
 
 }
+
+/// skip Already spent 3 weeks on this move on.
---
+++ src/_10_slot_machine/SlotMachine.java
@@ -0,0 +1,10 @@
+package _10_slot_machine;
+
+public class SlotMachine {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
