f035c0432b2fd3f59250e1dcc76a33638e26fe55
league <league@imac-24.attlocal.net>
2023-09-23 16:27:13 -0700

    Act finish the slot machine like cmon we gotta move on

--- src/_10_slot_machine/SlotMachine.java
+++ src/_10_slot_machine/SlotMachine.java
@@ -2,9 +2,13 @@ package _10_slot_machine;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.Random;
 
 import javax.imageio.ImageIO;
+import javax.swing.Icon;
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
@@ -15,39 +19,36 @@ public class SlotMachine implements ActionListener {
 	JFrame frame = new JFrame();
 	JPanel panel = new JPanel();
 	JButton spinButton = new JButton("Spin");
-	JLabel label;
+	JLabel label,label2, label3;
+	String cherry = "cherry.jpeg";
+	String lime = "lime.jpeg";
+	String orange = "orange.jpeg";
 	
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		String cherry = "cherry.jpeg";
-		String lime = "lime.jpeg";
-		String orange = "orange.jpeg";
-		int random = new Random().nextInt(2);
-		if(random == 0) {
-			label = loadImageFromComputer(cherry);
-
-		}else if(random == 1){
-			label = loadImageFromComputer(lime);
-
-		}else{
-			label = loadImageFromComputer(orange);
+	private JLabel createLabelImage(String fileName) throws MalformedURLException{
+        URL imageURL = getClass().getResource(fileName);
+	if (imageURL == null){
+		System.err.println("Could not find image " + fileName);
+		return new JLabel();
+	}
 	
-		}
+	Icon icon = new ImageIcon(imageURL);
+	JLabel imageLabel = new JLabel(icon);
+	return imageLabel;
 	}
-	private static Object loadImageFromComputer(String cherry) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-	public void makeButtons() {
+
+	public void makeGUI() {
+		randomizer();
+		panel.add(label);
+		panel.add(label2);
+		panel.add(label3);
 		frame.add(panel);
 		frame.setVisible(true);
 		frame.add(spinButton);
 		frame.setSize(100, 100);
-		
-		
-		
-		frame.pack();
 		frame.setTitle("Slot Machine");
+		frame.pack();
+		System.out.println("Hello, User. Please press the Spin Button to being your gambling addiction");
+		
 		
 	}
 	@Override
@@ -55,7 +56,48 @@ public class SlotMachine implements ActionListener {
 		// TODO Auto-generated method stub
 		JButton buttonPressed = (JButton) e.getSource();
 		if (buttonPressed == spinButton) {
-			
+			randomizer();
+		}
+	}
+	public void randomizer() {
+		try {
+			int random = new Random().nextInt(2);
+			if(random == 0) {
+				
+				label = createLabelImage(cherry);
+				
+			}else if(random == 1){
+				label = createLabelImage(lime);
+
+			}else{
+				label = createLabelImage(orange);
+		
+			}
+			int random2 = new Random().nextInt(2);
+			if(random2 == 0) {
+				label2 = createLabelImage(cherry);
+				
+			}else if(random2 == 1){
+				label2 = createLabelImage(lime);
+
+			}else{
+				label2 = createLabelImage(orange);
+		
+			}
+			int random3 = new Random().nextInt(2);
+			if(random3 == 0) {
+				label3 = createLabelImage(cherry);
+				
+			}else if(random3 == 1){
+				label3 = createLabelImage(lime);
+
+			}else{
+				label3 = createLabelImage(orange);
+		
+			}
+		} catch (MalformedURLException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
 		}
 	}
 	
